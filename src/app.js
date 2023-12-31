e212aa6b4c049a1e4a151cf867952eefb3a1655f
Yogesh <ybhatta70@gmail.com>
2023-12-31 11:29:26 +0530

    standard error and api response class created

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+
+const app = express()
+
+app.use(cors({
+    origin:process.env.CORS_ORIGIN,
+    credentials:true
+}))
+
+app.use(express.json({limit:'16kb'})) // only upto 16 kb of json data is accepted
+app.use(express.urlencoded({extended: true})) //extended means when data is coming from url, extended objects are allowed
+app.use(express.static("public"))
+app.use(cookieParser())
+
+
+
+export { app }
